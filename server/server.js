0a826bac4a54bd8103c67a2bc690218ab152dcc0
Luis Camilo <lhenriquegomescamilo@hotmail.com>
2017-06-01 22:18:45 -0300

    master: Configurando server

---
+++ server/server.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var http = require("http");
+var server = http.createServer();
+server.listen(3000, function () { return console.log('SERVER RUNNING ARE ON PORT 3000'); });
---
+++ server/server.ts
@@ -0,0 +1,5 @@
+import * as http from 'http';
+
+const server = http.createServer();
+
+server.listen(3000, () => console.log('SERVER RUNNING ARE ON PORT 3000'));
