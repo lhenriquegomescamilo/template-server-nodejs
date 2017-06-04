078ff29c17eaa344aabd053c7e07cf2fd6ca03f0
Luis Camilo <lhenriquegomescamilo@hotmail.com>
2017-06-04 00:29:27 -0300

    dev: configure routes of user

--- build/server/modules/user/routes.js
+++ build/server/modules/user/routes.js
@@ -1,17 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var user_controller_1 = require("./user-controller");
 var UserRoutes = (function () {
     function UserRoutes() {
+        this._userController = new user_controller_1.default();
     }
     UserRoutes.prototype.index = function (request, response) {
+        return this._userController.findAll(request, response);
     };
     UserRoutes.prototype.create = function (request, response) {
+        return this._userController.create(request, response);
     };
     UserRoutes.prototype.findOne = function (request, response) {
+        return this._userController.findById(request, response);
     };
     UserRoutes.prototype.updateOne = function (request, response) {
+        return this._userController.update(request, response);
     };
     UserRoutes.prototype.deleteOne = function (request, response) {
+        return this._userController.delete(request, response);
     };
     return UserRoutes;
 }());
--- server/modules/user/routes.ts
+++ server/modules/user/routes.ts
@@ -1,25 +1,29 @@
 import { Request, Response } from 'express';
+import UserController from './user-controller';
 
 class UserRoutes {
+    private _userController: UserController;
+
     constructor() {
+        this._userController = new UserController();
     }
 
     index(request: Request, response: Response) {
-        
+        return this._userController.findAll(request, response);
     }
 
     create(request: Request, response: Response) {
-
+        return this._userController.create(request, response);
 
     }
     findOne(request: Request, response: Response) {
-
+        return this._userController.findById(request, response);
     }
     updateOne(request: Request, response: Response) {
-
+        return this._userController.update(request, response);
     }
     deleteOne(request: Request, response: Response) {
-
+        return this._userController.delete(request, response);
     }
 }
 
